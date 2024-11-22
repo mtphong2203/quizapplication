dc8783a5e164a699cd9e279f840980f5a87372a2
Tien Phong <tienphong2203@gmail.com>
2024-11-22 13:32:27 +0700

    Step 1: Create base project

---
+++ src/test/java/com/maiphong/quizapplication/QuizapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maiphong.quizapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
